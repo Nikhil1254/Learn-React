cf037fa3a234757e00c5095cf6415806d1fd8794
Nikhil <np0129999@gmail.com>
2023-04-06 15:08:42 +0530

    counter example using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useReducer } from "react";
+
 function App() {
 
+    const [count, dispatch] = useReducer(countReducer, 0);
+
+    function countReducer(prevCount, action) {
+        switch (action.type) {
+            case "INCREMENT":
+                return prevCount + action.payload;
+            case "DECREMENT":
+                return prevCount > 0 ? prevCount - action.payload : prevCount;
+            default:
+                return count;
+        }
+    }
 
     return <>
-        <h1>App Component</h1>
+        <div className="container text-center mt-2">
+            <h1>{count}</h1>
+            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 1 })} className="btn btn-success mx-1 px-3">+1</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", payload: 1 })} className="btn btn-warning mx-1 px-3">-1</button>
+            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 5 })} className="btn btn-success mx-1 px-3">+5</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", payload: 5 })} className="btn btn-warning mx-1 px-3">-5</button>
+        </div>
     </>
 }
 
