981bd30b9d9a6ad38498168d603c9d2d0ae8f40a
Nikhil <np0129999@gmail.com>
2023-04-07 11:30:42 +0530

    added multiple functionalities in reducer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ function App() {
 
     const [count, dispatch] = useReducer(countReducer, 0);
 
-
+    console.log("render");
     return <>
         <div className="container text-center mt-2">
             <h1>{count}</h1>
             <button onClick={() => dispatch({ type: 'INCREMENT', payload: 1 })} className="btn btn-success mx-1 px-3">+1</button>
             <button onClick={() => dispatch({ type: "DECREMENT", payload: 1 })} className="btn btn-warning mx-1 px-3">-1</button>
             <button onClick={() => dispatch({ type: 'INCREMENT', payload: 5 })} className="btn btn-success mx-1 px-3">+5</button>
-            <button onClick={() => dispatch({ type: "DECREMENT", payload: 5 })} className="btn btn-warning mx-1 px-3">-5</button>
+
+            <button onClick={() => dispatch({ type: "MULTIPLY", payload: 2 })} className="btn btn-warning mx-1 px-3">x2</button>
+            <button onClick={()=>dispatch({type:"RESET"})} className="btn btn-danger">
+                Reset
+            </button>
         </div>
     </>
 }
--- src/reducers.js
+++ src/reducers.js
@@ -4,6 +4,10 @@ export function countReducer(prevCount, action) {
             return prevCount + action.payload;
         case "DECREMENT":
             return prevCount > 0 ? prevCount - action.payload : prevCount;
+        case "MULTIPLY":
+            return prevCount * action.payload;
+        case "RESET":
+            return 0;
         default:
             return prevCount;
     }
